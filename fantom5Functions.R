6ea2dab5f1097d103a337a6b5f6c7a13b0c0a3fc
Atticus29 <mark.aaron.fisher@gmail.com>
2016-09-09 10:58:07 -0700

    made fantom5function getTopXCellTypes take a matrix instead of data table argument so that it could convert stuff to numeric without having to use as.matrix for every loop. Also made it output the range in the fantom five table encountered with a hit as the names of the list elements that are matches

--- fantom5Functions.R
+++ fantom5Functions.R
@@ -1,7 +1,7 @@
-getTopXCellTypes = function(ff_dt=NULL, CHROMONUM, range_v, x){
-  if(is.null(ff_dt)){
+getTopXCellTypes = function(ff_mat=NULL, CHROMONUM, range_v, x){
+  if(is.null(ff_mat)){
     require(data.table)
-    ff_dt = fread("grep -v '^#' /home/exacloud/lustre1/CompBio/genomic_resources/fantom5/human/hg19.cage_peak_phase1and2combined_tpm_ann.osc.txt")
+    ff_mat = as.matrix(fread("grep -v '^#' /home/exacloud/lustre1/CompBio/genomic_resources/fantom5/human/hg19.cage_peak_phase1and2combined_tpm_ann.osc.txt"))
   }
   #check that range_v is valid
   if (length(range_v)!=2){
@@ -10,18 +10,20 @@ getTopXCellTypes = function(ff_dt=NULL, CHROMONUM, range_v, x){
   }else{
     #make sure it goes in correct order
     range_v = range(range_v)
-    #parse the first column of ff_dt, which has format of, chr10:100150986..100150988,+
-    annotationParsed_ls = strsplit(ff_dt[["00Annotation"]], split='chr|:|\\.\\.|,')
+    #parse the first column of ff_mat, which has format of, chr10:100150986..100150988,+
+    annotationParsed_ls = strsplit(ff_mat[,"00Annotation"], split='chr|:|\\.\\.|,')
     #find first list element with 5
     FIRST=1
-    while (length(annotationParsed_ls[[x]])<5) {
+    while (length(annotationParsed_ls[[FIRST]])<5) {
       FIRST = FIRST+1
     }
     matches_ls = list()
     for (locCounter in FIRST:length(annotationParsed_ls)){
       if(range_v[1] <= annotationParsed_ls[[locCounter]][4] & range_v[2] >= annotationParsed_ls[[locCounter]][3] & CHROMONUM==annotationParsed_ls[[locCounter]][2]){
-        inRangeEntry_ls = sort(as.matrix(ff_dt)[locCounter,grep('tpm', colnames(ff_dt))], decreasing=T, index.return=T) #this is a super time consuming step
-        matches_ls = list(matches_ls, inRangeEntry_ls[[1]][1:x])
+        inRangeEntry_ls = sort(as.numeric(ff_mat[locCounter,grep('tpm', colnames(ff_mat))]), decreasing=T, index.return=T) #this is a super time consuming step
+        names(inRangeEntry_ls[[1]]) = colnames(ff_mat)[grep('tpm', colnames(ff_mat))[inRangeEntry_ls[[2]]]]
+        matches_ls[[length(matches_ls)+1]] = inRangeEntry_ls[[1]][1:x]
+        names(matches_ls)[length(matches_ls)]=paste0(annotationParsed_ls[[locCounter]][2],":", annotationParsed_ls[[locCounter]][3],"-", annotationParsed_ls[[locCounter]][4])
         #return(inRangeEntry_ls[[1]][1:x])
       }
     }
