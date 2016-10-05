c62f7ca25e17bc8d57b829af6c2d7604528d8f0e
Atticus29 <mark.aaron.fisher@gmail.com>
2016-10-05 13:15:34 -0700

    Roxygen-ready documentation for FFTopNCellTypesWrapper as well as some more defensive programming for this function

--- fantom5Functions.R
+++ fantom5Functions.R
@@ -1,21 +1,55 @@
 FFTopNCellTypesWrapper = function(ff_dt, query_dt, ChrColName, StartColName, StopColName, N, savePath, colStrWithChromLocations_str="00Annotation"){ #, queryDtENSRidColName="ENSRid", queryDtRegDescriptionColName="gwasEnsemblRegDescription", queryDtVarLenAcrossENSRidColName="VarLenAcrossENSRid_x"){
+  #' Wrap the output of getTopNCellTypesDtVersionWithOutOfRangeReturns over a data.table of queries instead of one at a time and convert the output of that function into a data.table of cell type/sample names instead of the list of list of data.tables using the ConvertFFlsOflsOfDtToDt function.
+  #' 
+  #' @param ff_dt a data table of hg19.cage_peak_phase1and2combined_tpm_ann.osc.txt. If no data.table is provided, the function will look for the hard-coded path in exacloud.
+  #' @param query_dt a data.table with at least three columns: one corresponding to a chromosome number, one corresponding to the start position, and one correpsonding to the stop position. Rows populated with positions (e.g., regulatory element positions) of interest to the user.
+  #' @param ChrColName a string containing the name() of the column in query_dt containing the chromosome position information.
+  #' @param StartColName a string containing the name() of the column in query_dt containing the start position information.
+  #' @param StopColName a string containing the name() of the column in query_dt containing the stop position information.
+  #' @param N a numeric() argument the retreives the top N largest tpm-containing columns in a given row of ff_dt
+  #' @param savePath a string that specifies the destination directory for the two return items (a data table and list of list of data tables)
+  #' @return A list containing two elements: 1) a data table with ncol() N+2. The first column ("query_info") contains the chromosome, start, and stop positions of the query, separated by underscore delimiters. The second column ("match_info") contains the chromosome, start, and stop position of the INTERSECT region of the query position and any match within ff_dt. It also includes the original column name of its match in ff_dt following an "OriginalColName" string and an underscore delimiter. Failing a match, the match_info column will report the entire position of the nearest ff_dt TSS. If there are two nearest ff_dt TSSs, two separate matches will be reported, each reporting the entire position of its ff_dt TSS. Note that the query position may intersect more than one ff_dt TSS. In this case, each intersect will be reported as a separate row in the output. The output data.table will therefore have nrow() >= nrow(query_dt). 2) A list of list of data tables. The length() of the list will be nrow(query_dt). Each element of the list will contain a list of data tables, where each element of this list will represent one match (as previously mentioned, there will always be at least one "match"--direct or nearest--and sometimes more than one match). Each match will correspond to a data.table with nrow()=1 and ncol()=N. The names() of the data.table will correspond to the cell types/samples in ff_dt, and the entries in the table will be tpm counts from ff_dt, sorted from highest to lowest. If a chromosome number in the query data table doesn't match a chromosome number in ff_dt, it simply gets skipped.
+  #' @examples
+  #' ffDummy_dt = data.table(Annotation=c("chr10:10..20,-", "chr10:25..30,-","chr10:35..100,-","chr10:106..205,-","chr10:223..250,-","chr10:269..478,-","chr10:699..1001,-","chr10:2000..2210,-","chr10:2300..2500,-","chr10:2678..5678,-"),tpmOne=c(0,0,0.213,1,1.2,0.5,0.7,0.9,0.8,0.86), tpmTwo=c(100,1000,1001,1500,900,877,1212,1232,1312,0),tpmThree=c(0.2138595,0,0,0,0,0,0.6415786,0,0,0))
+  #' query_dt = data.table(chrP = c(10,10,10,10,11), startP = c(1,31,1,10000,1), stopP=c(10000,33,3,10030,100))
+  #' output_ls = FFTopNCellTypesWrapper(ff_dt = ffDummy_dt, query_dt = query_dt, ChrColName = "chrP", StartColName ="startP", StopColName = "stopP", N = 2, savePath = "~/Desktop/", colStrWithChromLocations_str="Annotation")
+  
+  
+  #If ff_dt was not provided, try to load it from its home in exacloud #Attn hardcoded
   if(is.null(ff_dt)){ #unchecked
     require(data.table)
     ff_dt = fread("grep -v '^#' /home/exacloud/lustre1/CompBio/genomic_resources/fantom5/human/hg19.cage_peak_phase1and2combined_tpm_ann.osc.txt")
   }
+  
+  #Check that ff_dt and query_dt are data.tables. If not, try to coerce them to data.tables
+  if(! (is.data.table(ff_dt) & is.data.table(query_dt))){
+    ff_dt = as.data.table(ff_dt)
+    query_dt = as.data.table(query_dt)
+  }
+  
+  #Initiate and populate the list of list of data.tables
   FFtopNcells_lsOfdt_ls = list()
   for(r in 1:nrow(query_dt)){
-    if(r%%100==0) {print(r)}
+    if(r%%50==0) {print(r)} #print r every 50th iteration of the loop to reassure an eager analyst
     FFtopNcells_lsOfdt_ls[[r]] = getTopNCellTypesDtVersionWithOutOfRangeReturns(ff_dt=ff_dt,chromoNum=as.numeric(query_dt[r,get(ChrColName)]), range_v=c(as.numeric(query_dt[r,get(StartColName)]),as.numeric(query_dt[r,get(StopColName)])), N=N, colStrWithChromLocations_str=colStrWithChromLocations_str)
-    #names(FFtopNcells_lsOfdt_ls)[r] = paste(query_dt[r,get(queryDtENSRidColName)],query_dt[r,get(queryDtRegDescriptionColName)], query_dt[r,get(ChrColName)],query_dt[r,get(StartColName)],query_dt[r,get(StopColName)],"VarLenAcrossENSRid",query_dt[r,get(queryDtVarLenAcrossENSRidColName)],sep="_") #Attn. This line might could change to be more generalizeable
-    names(FFtopNcells_lsOfdt_ls)[r] = paste(query_dt[r,get(ChrColName)],query_dt[r,get(StartColName)],query_dt[r,get(StopColName)],"query",sep="_")
+    
+    #if the r-th element of the list was successfully populated (it wouldn't be if, say, there was no chromosome number in ff_dt that matched the query), let's go ahead and give that element a name
+    if(length(FFtopNcells_lsOfdt_ls) ==r){
+      names(FFtopNcells_lsOfdt_ls)[r] = paste(query_dt[r,get(ChrColName)],query_dt[r,get(StartColName)],query_dt[r,get(StopColName)],"query",sep="_")
+    }
   }
+  
+  #Save the list of list of data.tables to a text file. Not for client viewing, but useful for analyst to check.
   sink(file=paste0(savePath,"FFtop",N,"cells_lsOfdt_ls.txt"))
   print(FFtopNcells_lsOfdt_ls)
   sink()
-  z=ConvertFFlsOflsOfDtToDt(FFtopNcells_lsOfdt_ls,N)
-  write.table(z, file=paste0(savePath, "Fantom5Top",N,"CellTypes.txt"), sep="\t", row.names = F)
-  output_ls = list(z, FFtopNcells_lsOfdt_ls)
+  
+  #Convert the list of list of data.tables to one data.table more amenable to downstream analyses
+  Converted_dt=ConvertFFlsOflsOfDtToDt(FFtopNcells_lsOfdt_ls,N)
+  write.table(Converted_dt, file=paste0(savePath, "Fantom5Top",N,"CellTypes.txt"), sep="\t", row.names = F)
+  
+  #Function will return both the data.table Converted_dt and the list of list of data.tables.
+  output_ls = list(Converted_dt, FFtopNcells_lsOfdt_ls)
   names(output_ls) = c(paste0("Data table of query and match output for top ",N," cell types" ), "List of list of data tables output containing actual tpms")
   return(output_ls)
 }
@@ -114,65 +148,80 @@ getTopNCellTypesDtVersionWithOutOfRangeReturns = function(ff_dt,chromoNum, range
   #range_v is a numeric() vector containing the Start and Stop bp position of the query (e.g., range_v = c(100,123))
   #N is a numeric() argument the retreives the top N largest tpm-containing columns in a given row of ff_dt
   #colStrWithChromLocations_str is a string containing the name() of the column in ff_dt containing the position information. In the ff_dt, note that the position information needs to be parsed out, which this function('s dependency) does automatically. Default is "00Annotation".
-  if(is.null(ff_dt)){ #unchecked
-    require(data.table)
-    ff_dt = fread("grep -v '^#' /home/exacloud/lustre1/CompBio/genomic_resources/fantom5/human/hg19.cage_peak_phase1and2combined_tpm_ann.osc.txt")
-  }
-  ffSorted_dt = getSortedDTwithSplitOfFirstCol(ff_dt=copy(ff_dt), nameOfCol1=colStrWithChromLocations_str)
-  #check that range_v is valid
-  if (length(range_v)!=2){
-    print("Too few or two many items in range_v. Should be a vector of length 2")
+  
+  #Check that N isn't greater than the number of cell types/samples available
+  if(N > length(grep('tpm', names(ff_dt)))){
     return(NULL)
   }else{
-    #make sure it goes in correct order
-    range_v = range(range_v)
-    #Find the first row where Start of our query is >= Start in dt
-    chromHits = which(ffSorted_dt[,chr]==as.character(chromoNum))
-    startRowChrom = chromHits[1]
-    endRowChrom = chromHits[length(chromHits)]
-    directHit = which(ffSorted_dt[startRowChrom:endRowChrom,Stop]>=range_v[1] & range_v[2]>= ffSorted_dt[startRowChrom:endRowChrom,Start])
-    #I think this has to be a for loop, because I have to sort each row independently, and there's no way to retain colnames under those circumstances
-    #topCells = t(apply(ffSorted_dt[startRowChrom:endRowChrom,][directHit,grep('tpm', names(ffSorted_dt)),with=F],1,sort))
-    #topCells=sapply(ffSorted_dt[startRowChrom:endRowChrom,][directHit,grep('tpm', names(ffSorted_dt)),with=F],function(x)order(x,decreasing=TRUE))
-    #topCells[1:N]
-    if(length(directHit)>0){
-      matches_ls = list()
-      for (dh in 1:length(directHit)){
-        topCells_dt = sort(ffSorted_dt[startRowChrom:endRowChrom,][directHit[dh],grep('tpm', names(ffSorted_dt)),with=F], decreasing = TRUE)[,1:N,with=F]
-        #topCellsName_v = names(topCells_dt)
-        matches_ls[[length(matches_ls)+1]] = topCells_dt
-        names(matches_ls)[length(matches_ls)] = paste(chromoNum,max(range_v[1],ffSorted_dt[startRowChrom:endRowChrom,][directHit[dh],Start]),min(range_v[2],ffSorted_dt[startRowChrom:endRowChrom,][directHit[dh],Stop]),"matched_region_OriginalColName",ffSorted_dt[startRowChrom:endRowChrom,][directHit[dh],][[colStrWithChromLocations_str]],sep="_")
-      }
-      return(matches_ls)
+    
+    if(is.null(ff_dt)){ #unchecked
+      require(data.table)
+      ff_dt = fread("grep -v '^#' /home/exacloud/lustre1/CompBio/genomic_resources/fantom5/human/hg19.cage_peak_phase1and2combined_tpm_ann.osc.txt")
     }
-    if(length(directHit)==0){
-      ##Look for whether the query start is beyond the very last Stop in ffSorted_dt for this chromosome
-      if (range_v[1] > ffSorted_dt[startRowChrom:endRowChrom,][nrow(ffSorted_dt[startRowChrom:endRowChrom,]),Stop]){
-        nearestUpstreamNeighborIdx = nrow(ffSorted_dt[startRowChrom:endRowChrom,])
-        nearestDownstreamNeighborIdx = nearestUpstreamNeighborIdx
+    ffSorted_dt = getSortedDTwithSplitOfFirstCol(ff_dt=copy(ff_dt), nameOfCol1=colStrWithChromLocations_str)
+    
+    #check that range_v is valid
+    if (length(range_v)!=2){
+      print("Too few or two many items in range_v. Should be a vector of length 2")
+      return(NULL)
+    }else{
+      #make sure the range goes in correct order
+      range_v = range(range_v)
+      
+      #Find the first row where Start of our query is >= Start in dt
+      chromHits = which(ffSorted_dt[,chr]==as.character(chromoNum))
+      if(length(chromHits)<1){
+        print(paste0("Invalid or non-matching chromosome number: ",chromoNum))
+        #browse()
+        return(NULL)
       }else{
-        ##Look for first occassion where Start in the ffSort_dt is greater than Stop in the query
-        nearestDownstreamNeighborIdx = which(ffSorted_dt[startRowChrom:endRowChrom,Start]>range_v[2])[1]
-        if (nearestDownstreamNeighborIdx>1){
-          nearestUpstreamNeighborIdx = nearestDownstreamNeighborIdx-1
-        }else{
-          nearestUpstreamNeighborIdx=nearestDownstreamNeighborIdx
+        startRowChrom = chromHits[1]
+        endRowChrom = chromHits[length(chromHits)]
+        directHit = which(ffSorted_dt[startRowChrom:endRowChrom,Stop]>=range_v[1] & range_v[2]>= ffSorted_dt[startRowChrom:endRowChrom,Start])
+        #I think this has to be a for loop, because I have to sort each row independently, and there's no way to retain colnames under those circumstances
+        #topCells = t(apply(ffSorted_dt[startRowChrom:endRowChrom,][directHit,grep('tpm', names(ffSorted_dt)),with=F],1,sort))
+        #topCells=sapply(ffSorted_dt[startRowChrom:endRowChrom,][directHit,grep('tpm', names(ffSorted_dt)),with=F],function(x)order(x,decreasing=TRUE))
+        #topCells[1:N]
+        if(length(directHit)>0){
+          matches_ls = list()
+          for (dh in 1:length(directHit)){
+            topCells_dt = sort(ffSorted_dt[startRowChrom:endRowChrom,][directHit[dh],grep('tpm', names(ffSorted_dt)),with=F], decreasing = TRUE)[,1:N,with=F]
+            #topCellsName_v = names(topCells_dt)
+            matches_ls[[length(matches_ls)+1]] = topCells_dt
+            names(matches_ls)[length(matches_ls)] = paste(chromoNum,max(range_v[1],ffSorted_dt[startRowChrom:endRowChrom,][directHit[dh],Start]),min(range_v[2],ffSorted_dt[startRowChrom:endRowChrom,][directHit[dh],Stop]),"matched_region_OriginalColName",ffSorted_dt[startRowChrom:endRowChrom,][directHit[dh],][[colStrWithChromLocations_str]],sep="_")
+          }
+          return(matches_ls)
         }
-      }
-      #is the Stop of the nearestUpstreamNeighbor closer to start of the query, or is the Start of nearestDownstreamNeighbor closer to the stop of the query?
-      QstopDiffDownStart = ffSorted_dt[startRowChrom:endRowChrom,][nearestDownstreamNeighborIdx,Start] - range_v[2]
-      QstartDiffUpStop= range_v[1] - ffSorted_dt[startRowChrom:endRowChrom,][nearestUpstreamNeighborIdx,Stop]
-      if (QstopDiffDownStart < QstartDiffUpStop){
-        newPositions = range(ffSorted_dt[startRowChrom:endRowChrom,][nearestDownstreamNeighborIdx,Start], ffSorted_dt[startRowChrom:endRowChrom,][nearestDownstreamNeighborIdx,Stop])
-        return(getTopNCellTypesDtVersionWithOutOfRangeReturns(ff_dt=ff_dt,chromoNum=chromoNum, range_v=newPositions, N=N, colStrWithChromLocations_str=colStrWithChromLocations_str))
-      }
-      if(QstartDiffUpStop < QstopDiffDownStart){
-        newPositions = range(ffSorted_dt[startRowChrom:endRowChrom,][nearestUpstreamNeighborIdx,Start], ffSorted_dt[startRowChrom:endRowChrom,][nearestUpstreamNeighborIdx,Stop])
-        return(getTopNCellTypesDtVersionWithOutOfRangeReturns(ff_dt=ff_dt,chromoNum=chromoNum, range_v=newPositions, N=N, colStrWithChromLocations_str=colStrWithChromLocations_str))
-      }
-      if(QstopDiffDownStart == QstartDiffUpStop){
-        newPositions = range(ffSorted_dt[startRowChrom:endRowChrom,][nearestDownstreamNeighborIdx,Stop],ffSorted_dt[startRowChrom:endRowChrom,][nearestUpstreamNeighborIdx,Start])
-        return(getTopNCellTypesDtVersionWithOutOfRangeReturns(ff_dt=ff_dt,chromoNum=chromoNum, range_v=newPositions, N=N, colStrWithChromLocations_str=colStrWithChromLocations_str))
+        if(length(directHit)==0){
+          ##Look for whether the query start is beyond the very last Stop in ffSorted_dt for this chromosome
+          if (range_v[1] > ffSorted_dt[startRowChrom:endRowChrom,][nrow(ffSorted_dt[startRowChrom:endRowChrom,]),Stop]){
+            nearestUpstreamNeighborIdx = nrow(ffSorted_dt[startRowChrom:endRowChrom,])
+            nearestDownstreamNeighborIdx = nearestUpstreamNeighborIdx
+          }else{
+            ##Look for first occassion where Start in the ffSort_dt is greater than Stop in the query
+            nearestDownstreamNeighborIdx = which(ffSorted_dt[startRowChrom:endRowChrom,Start]>range_v[2])[1]
+            if (nearestDownstreamNeighborIdx>1){
+              nearestUpstreamNeighborIdx = nearestDownstreamNeighborIdx-1
+            }else{
+              nearestUpstreamNeighborIdx=nearestDownstreamNeighborIdx
+            }
+          }
+          #is the Stop of the nearestUpstreamNeighbor closer to start of the query, or is the Start of nearestDownstreamNeighbor closer to the stop of the query?
+          QstopDiffDownStart = ffSorted_dt[startRowChrom:endRowChrom,][nearestDownstreamNeighborIdx,Start] - range_v[2]
+          QstartDiffUpStop= range_v[1] - ffSorted_dt[startRowChrom:endRowChrom,][nearestUpstreamNeighborIdx,Stop]
+          if (QstopDiffDownStart < QstartDiffUpStop){
+            newPositions = range(ffSorted_dt[startRowChrom:endRowChrom,][nearestDownstreamNeighborIdx,Start], ffSorted_dt[startRowChrom:endRowChrom,][nearestDownstreamNeighborIdx,Stop])
+            return(getTopNCellTypesDtVersionWithOutOfRangeReturns(ff_dt=ff_dt,chromoNum=chromoNum, range_v=newPositions, N=N, colStrWithChromLocations_str=colStrWithChromLocations_str))
+          }
+          if(QstartDiffUpStop < QstopDiffDownStart){
+            newPositions = range(ffSorted_dt[startRowChrom:endRowChrom,][nearestUpstreamNeighborIdx,Start], ffSorted_dt[startRowChrom:endRowChrom,][nearestUpstreamNeighborIdx,Stop])
+            return(getTopNCellTypesDtVersionWithOutOfRangeReturns(ff_dt=ff_dt,chromoNum=chromoNum, range_v=newPositions, N=N, colStrWithChromLocations_str=colStrWithChromLocations_str))
+          }
+          if(QstopDiffDownStart == QstartDiffUpStop){
+            newPositions = range(ffSorted_dt[startRowChrom:endRowChrom,][nearestDownstreamNeighborIdx,Stop],ffSorted_dt[startRowChrom:endRowChrom,][nearestUpstreamNeighborIdx,Start])
+            return(getTopNCellTypesDtVersionWithOutOfRangeReturns(ff_dt=ff_dt,chromoNum=chromoNum, range_v=newPositions, N=N, colStrWithChromLocations_str=colStrWithChromLocations_str))
+          }
+        }
       }
     }
   }
@@ -220,6 +269,9 @@ getTopXCellTypes = function(ff_mat=NULL, chromoNum, range_v, x, colStrWithChromL
 # getTopNCellTypesDtVersionWithOutOfRangeReturns(ff_dt=ffDummy_dt,chromoNum=10, range_v=c(31,33), N=2, colStrWithChromLocations_str="Annotation")
 # getTopNCellTypesDtVersionWithOutOfRangeReturns(ff_dt=ffDummy_dt,chromoNum=10, range_v=c(1,3), N=2, colStrWithChromLocations_str="Annotation")
 # getTopNCellTypesDtVersionWithOutOfRangeReturns(ff_dt=ffDummy_dt,chromoNum=10, range_v=c(10000,10030), N=2, colStrWithChromLocations_str="Annotation")
+ffDummy_dt = data.table(Annotation=c("chr10:10..20,-", "chr10:25..30,-","chr10:35..100,-","chr10:106..205,-","chr10:223..250,-","chr10:269..478,-","chr10:699..1001,-","chr10:2000..2210,-","chr10:2300..2500,-","chr10:2678..5678,-"),tpmOne=c(0,0,0.213,1,1.2,0.5,0.7,0.9,0.8,0.86), tpmTwo=c(100,1000,1001,1500,900,877,1212,1232,1312,0),tpmThree=c(0.2138595,0,0,0,0,0,0.6415786,0,0,0))
+query_dt = data.table(chrP = c(10,10,10,10,11), startP = c(1,31,1,10000,1), stopP=c(10000,33,3,10030,100))
+FFTopNCellTypesWrapper(ff_dt = ffDummy_dt, query_dt = query_dt, ChrColName = "chrP", StartColName ="startP", StopColName = "stopP", N = 2, savePath = "~/Desktop/", colStrWithChromLocations_str="Annotation")
 
 # numericPartOfFf_mat = data.matrix(ffDummy_dt[,grep('tpm', colnames(ffDummy_dt)),with=F])
 # ffRanks_mat = matrix(nrow=(nrow(numericPartOfFf_mat)), ncol=length(grep('tpm', colnames(ffDummy_dt))))
